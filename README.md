40ef6429eb7e29adb0cae4eba335d3cfacc7c93f
Alex Gherghisan <alexghr@users.noreply.github.com>
2023-05-25 18:31:39 +0100

    feat: package all themes in alacritty/alacritty-theme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # alacritty-theme.nix
+
+This repo packages all of the themes available at [alacritty/alacritty-theme](https://github.com/alacritty/alacritty-theme)
+as individual flake outputs for easy use in your nix-managed Alacritty config.
+
+## Outputs
+
+Each YAML file in the [alacritty-theme/themes](https://github.com/alacritty/alacritty-theme/tree/0fb8868d6389014fd551851df7153e4ca2590790/themes) folder is exported as a package.
+
+Furthermore an overlay is provided that groups all of these themes under a single attribute set: `pkgs.alacritty-theme`.
+
+## Using a theme
+
+Add this repo to your inputs:
+
+```nix
+# flake.nix
+{
+  inputs.alacritty-theme.url = "github:alexghr/alacritty-theme.nix";
+}
+```
+
+Install the default overlay. This will make `pkgs.alacritty-theme` available:
+
+```nix
+{
+  nixpkgs.overlays = [ alacritty-theme.overlays.default ];
+}
+```
+
+Finally, import a theme in your alacritty config:
+
+```nix
+# using home-manager
+{
+  home-manager.users.you = hm: {
+    programs.alacritty = {
+      enable = true;
+      settings = {
+        import = [
+          pkgs.alacritty-theme.cyber_punk_neon
+        ];
+      };
+    };
+  };
+}
+```
+
+That's it :smile:
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "alacritty/alacritty-theme packaged for Nix consumption";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    alacritty-theme = {
+      url = "github:alacritty/alacritty-theme";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, flake-parts, alacritty-theme, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [];
+      flake = {
+        overlays.alacritty-theme = final: prev: {
+          alacritty-theme = self.packages.${prev.system};
+        };
+        overlays.default = self.overlays.alacritty-theme;
+      };
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      perSystem = {  pkgs, ... }:
+        let
+          isYaml = file: pkgs.lib.hasSuffix ".yaml" file || pkgs.lib.hasSuffix ".yml" file;
+          withoutYamlExtension = file: pkgs.lib.removeSuffix ".yml" (pkgs.lib.removeSuffix ".yaml" file);
+          dirEntries = builtins.attrNames (builtins.readDir "${alacritty-theme}/themes");
+          themeFiles = builtins.filter isYaml dirEntries;
+          themeDerivations = builtins.map (file: rec {
+            name = withoutYamlExtension file;
+            value = pkgs.stdenv.mkDerivation {
+              inherit name;
+              phases = [ "installPhase" ];
+              installPhase = ''
+                cp ${alacritty-theme}/themes/${file} $out
+              '';
+            };
+          }) themeFiles;
+        in {
+          packages = builtins.listToAttrs themeDerivations;
+        };
+    };
+}
