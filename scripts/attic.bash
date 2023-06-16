3adf23d0587cd28e4d5558a71a841de11b10590d
Alex Gherghisan <alexghr@users.noreply.github.com>
2023-06-16 17:34:53 +0100

    refactor: grab current system from nix

--- scripts/attic.bash
+++ scripts/attic.bash
@@ -2,10 +2,12 @@
 
 set -euf -o pipefail
 
+system="$(nix eval --impure --raw --expr 'builtins.currentSystem')"
+
 attic login default ${ATTIC_URL} ${ATTIC_AUTH_TOKEN}
 attic use ${ATTIC_CACHE}
 
-packages=$(nix flake show --json | jq '.packages."x86_64-linux" | keys | .[]' | tr -d \")
+packages=$(nix flake show --json | jq ".packages.\"$system\" | keys | .[]" | tr -d \")
 for package in $packages; do
   nix build .#$package
   attic push ${ATTIC_CACHE} ./result
